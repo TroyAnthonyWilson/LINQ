bb3cc77ab1d7d6d85209b6bc90b7565ffec8c2d5
Troy Wilson <Wilson.Troy.Anthony@gmail.com>
2022-07-18 10:29:22 -0400

    Add project files.

---
+++ LINQ/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace LINQ
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] nums = { 10, 2330, 112233, 12, 949, 3764, 2942, 523863 };
+            int evenNumbers = nums.Count(n => n % 2 == 0);
+            int minimum = nums.Min(n => n);
+            int maximum = nums.Max(n => n); 
+            int[] bigNumbers = nums.Where(n => n >= 100000 && n <= 999999).ToArray();
+            int[] tentoOneHundred = nums.Where(n => n > 10 && n < 100).ToArray();
+            int maximumAndLess = nums.Where(n => n < 10000).Max(n => n);
+
+            Console.WriteLine($"The Minimum value is {minimum}");
+            Console.WriteLine($"The Maximum value is {maximum}");
+            Console.WriteLine($"The Maximum value less then 10000 is {maximumAndLess}");
+            Console.Write("The numbers between 10 and 100 are: ");
+            foreach(int num in tentoOneHundred)
+            {
+                Console.Write($"{num} ");
+            }
+            Console.Write("\nThe inclusive numbers between 100000 and 999999 are: ");
+            foreach(int num in bigNumbers)
+            {
+                Console.Write($"{num} ");
+            }
+            Console.WriteLine($"\nThere are {evenNumbers} even numbers in the array");
+        }
+    }
+}
---
+++ LINQ/ReadMe.txt
@@ -0,0 +1,17 @@
+﻿LINQ
+Objective: LINQ and Collections
+
+Task: Use Linq to search the following collection.
+
+Build Specifications:
+Create the following array
+int[] nums = { 10, 2330, 112233, 12, 949, 3764, 2942, 523863 };
+
+Write code using Linq to answer the following questions: 
+
+Find the Minimum value 
+Find the Maximum value
+Find the Maximum value less than 10000
+Find all the values between 10 and 100
+Find all the Values between 100000 and 999999 inclusive 
+Count all the even numbers 
